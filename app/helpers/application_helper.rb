b2e872361e989379742f72d58f8f400d683ba506
Jan Carlo <jcgalbano@gmail.com>
2017-05-23 03:52:15 +0800

    Setup all pages and add CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(s='')
+    if s.empty?
+      return "Ruby on Rails Tutorial Sample App"
+    else
+      return s+" | "+"Ruby on Rails Tutorial Sample App"
+    end
+  end
 end
